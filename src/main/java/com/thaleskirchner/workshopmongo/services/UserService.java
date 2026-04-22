d557e3dbd7db69c7c8598c20acd949a5f270c758
Thales Kirchner Ozéias <thales.ozeias@gmail.com>
2026-04-22 17:06:40 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/thaleskirchner/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.thaleskirchner.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thaleskirchner.workshopmongo.domain.User;
+import com.thaleskirchner.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+}
