ee0f5369dff774fa52f00b85a5eef6cf60d522c4
Thales Kirchner Ozéias <thales.ozeias@gmail.com>
2026-05-04 14:53:00 -0300

    Query with multiple parameters

--- src/main/java/com/thaleskirchner/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/thaleskirchner/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.thaleskirchner.workshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,4 +16,11 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: { $gte: ?1 } }, " //minDate
+			+ "{ date: { $lte: ?2 } }, " //maxDate
+			+ "{ $or: [ { 'title': { $regex: ?0, $options: 'i' } }, "
+			+ "{ 'body': { $regex: ?0, $options: 'i' } }, " // find text in body 
+			+ "{ 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }") // find text in comments 
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
